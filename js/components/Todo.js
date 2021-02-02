6e688832ff880144a72755a9420131ad5c53ef98
Renata <76951689+Amoseta@users.noreply.github.com>
2021-02-02 12:13:46 +0200

    Methods

--- js/components/Todo.js
+++ js/components/Todo.js
@@ -1,4 +1,4 @@
-class Todo {
+/* class Todo {
     constructor(pavadinimas) {
         this.name = pavadinimas;
         this.list = [];
@@ -15,3 +15,198 @@ class Todo {
 }
 
 export { Todo }
+ */
+
+/* class Suo {
+    constructor(Ciupkus) {
+        this.greet = Ciupkus;
+        this.fur = ['juodai rudas'];
+    }
+
+    summary() {
+        const text = `Hello, I am ${this.greet} and my fur is ${this.fur}.`;
+        console.log(text);
+    }
+    add(text) {
+        this.fur.push(text);
+    }
+}
+export { Suo }
+*/
+
+class Todo {
+    constructor(pavadinimas) {
+        this.name = pavadinimas;
+        this.list = [];
+    }
+
+    summary() {
+        const text = `${this.name} sarase yra ${this.list.length} uzduotys.`; 
+        console.log(text);
+    }
+
+    add(text) {
+        this.list.push({
+            text: text,
+            completed: false
+        });
+
+    edit(index, newText) {
+        this.list[index].text = newText;
+    }
+
+    remove(index) {
+        if (index < 0 ||
+            index >= this.list.length) {
+            console.error('ERROR');
+            return;    
+        }
+    }
+        const filtered = [];
+
+        for (let i = 0; i < this.list.length; i++) {
+            if (i !== index) {
+                filtered.push(this.list[i]);
+            }
+        }
+        this.list = filtered;
+    }
+
+    print(list = this.list) {
+         console.table(list);
+    }
+
+    taskCompleted(index) {
+        this.list[index].completed = true;
+    }
+
+    printCompletedOnly() {
+        const completed = [];
+
+        for (const task of this.list) {
+            if (task.completed) {
+                completed.push(task);
+            }
+        }
+        this.print(completed);
+    }
+
+    printUnCompletedOnly() {
+        const uncompleted = [];
+
+        for (const task of this.list) {
+            if (!task.completed) {
+                uncompleted.push(task);
+            }
+        }
+        this.print(uncompleted);
+    }
+}
+
+export { Todo }
+
+
+
+/*
+function daugyba(a = 1, b = 1) {
+    return a * b;
+}
+
+console.log(daugyba(4, 5));
+console.log(daugyba(4));
+
+console.log(daugyba(2, 7));
+console.log(daugyba(2));
+
+console.log(daugyba());
+
+
+
+function labas(name = 'zmogau') {
+    console.log(`Labas, ${name}!`)
+}
+
+labas('Petrai')
+labas('Onute')
+labas()
+
+
+// arrow function 
+
+function suma(a, b) {
+    return a + b;
+}
+console.log(suma(4, 5));
+
+const skirtumas = function (a, b) {
+    return a - b;
+}
+console.log(skirtumas(4, 5));
+
+const daugyba = (a, b) => {
+    return a * b;
+}
+console.log(daugyba(4, 5));
+
+const dalyba = (a, b) => a / b;
+console.log(dalyba(4, 5));
+
+const kvadratu = a => a * a;
+console.log(kvadratu(4));
+
+
+
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(a);
+
+const aKvadratu = [];
+for (let i = 0; i < a.length; i++) {
+    aKvadratu.push(a[i] * a[i])
+}
+console.log(aKvadratu);
+
+const bMap = a.map(n => n * n);
+console.log(bMap);
+
+const lygyniai = [];
+for (let i = 0; i < a.length; i++) {
+    if (a[i] % 2 === 0) {
+        lygyniai.push(a[i]);
+    }
+}
+console.log(lygyniai);
+
+const cFilter = a.filter(n => n % 2 === 0);
+console.log(cFilter);
+
+const lygyniuKvadarai = [];
+for (let i = 0; i < a.length; i++) {
+    if (a[i] % 2 === 0) {
+        lygyniuKvadarai.push(a[i] * a[i]);
+    }
+}
+console.log(lygyniuKvadarai);
+
+const dMapFilter = a.filter(n => n % 2 === 0).map(n => n * n);
+console.log(dMapFilter);
+
+klase: Car
+
+parametrai:
+    - ijungta: true/false
+    - bake: 5       (litrai)
+    - ryja: 2.5     (litrai/100 km)
+    - rida: 0
+
+car.turnOn()
+car.drive(20)
+car.drive(20)
+car.drive(20)
+car.drive(20)
+car.drive(20)
+car.drive(90)
+car.drive(100)  // varyklis issijungia, rodo klaida ir rida pasipildo proporcingai nuvaziuotai kelio daliai
+car.turnOff()
+
+*/
+ 
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { Todo } from './components/Todo.js';
+/* import { Todo } from './components/Todo.js';
 
 const randomUzduotys = new Todo('Random uzduotys');
 
@@ -10,3 +10,53 @@ randomUzduotys.add('Sukurti pirma uzduoti');
 randomUzduotys.add('uzmaisyti tesla');
 randomUzduotys.add('nueiti i parduotuve');
 randomUzduotys.summary();
+*/ 
+
+
+/* klase: Suo
+
+parametrai:
+    - vardas
+    - kailio spalva
+
+metodai:
+    - prisistatymas (Hello, I am {name} and my fur is {color} 🐕‍🦺)
+
+    
+    import { Suo } from './components/Todo.js';
+    
+    const greeting = new Suo('Ciupkus', 'juodai rudas');
+    
+    greeting.summary();
+    
+    greeting.add('su');
+    greeting.add('baltom');
+    greeting.add('demem');
+    greeting.summary();
+    
+*/
+
+import { Todo } from './components/Todo.js';
+
+const blynai = new Todo('Blynu kepimas');
+
+blynai.add('Uzmaisyti tesla');
+blynai.add('Ijungti kaitlente');
+blynai.add('Istraukti keptuve');
+blynai.add('Ipilti tesla i keptuve');
+blynai.add('Apversti blynus');
+blynai.add('Isimti blynus');
+blynai.add('Isjungti keptuve');
+
+blynai.edit(0, 'Isplauti dubeni ir uzmaisyti tesla');
+
+blynai.remove(4);
+blynai.taskCompleted (0);
+blynai.taskCompleted (2);
+blynai.taskCompleted (5);
+
+blynai.summary();
+
+blynai.print();
+blynai.printCompletedOnly();
+blynai.printUnCompletedOnly();
